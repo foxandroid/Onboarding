44cca96fa6e41bf2180018f82ab1dd1942f2b4a0
Robin Singla <robin.singla@asahiindia.com>
2021-02-12 17:59:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/onboarding/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager mSLideViewPager;
+    LinearLayout mDotLayout;
+    Button backbtn, nextbtn, skipbtn;
+
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        backbtn = findViewById(R.id.backbtn);
+        nextbtn = findViewById(R.id.nextbtn);
+        skipbtn = findViewById(R.id.skipButton);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) > 0){
+
+                    mSLideViewPager.setCurrentItem(getitem(-1),true);
+
+                }
+
+            }
+        });
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) < 3)
+                    mSLideViewPager.setCurrentItem(getitem(1),true);
+                else {
+
+                    Intent i = new Intent(MainActivity.this,mainscreen.class);
+                    startActivity(i);
+                    finish();
+
+                }
+
+            }
+        });
+
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(MainActivity.this,mainscreen.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+        mSLideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSLideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpindicator(0);
+        mSLideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void setUpindicator(int position){
+
+        dots = new TextView[4];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0 ; i < dots.length ; i++){
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive,getApplicationContext().getTheme()));
+            mDotLayout.addView(dots[i]);
+
+        }
+
+        dots[position].setTextColor(getResources().getColor(R.color.active,getApplicationContext().getTheme()));
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            setUpindicator(position);
+
+            if (position > 0){
+
+                backbtn.setVisibility(View.VISIBLE);
+
+            }else {
+
+                backbtn.setVisibility(View.INVISIBLE);
+
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private int getitem(int i){
+
+        return mSLideViewPager.getCurrentItem() + i;
+    }
+
+}
---
+++ app/src/main/java/com/example/onboarding/ViewPagerAdapter.java
@@ -0,0 +1,86 @@
+package com.example.onboarding;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+
+    int images[] = {
+
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4
+
+    };
+
+    int headings[] = {
+
+            R.string.heading_one,
+            R.string.heading_two,
+            R.string.heading_three,
+            R.string.heading_fourth
+    };
+
+    int description[] = {
+
+      R.string.desc_one,
+      R.string.desc_two,
+      R.string.desc_three,
+      R.string.desc_fourth
+    };
+
+    public ViewPagerAdapter(Context context){
+
+        this.context = context;
+
+    }
+
+    @Override
+    public int getCount() {
+        return  headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_layout,container,false);
+
+        ImageView slidetitleimage = (ImageView) view.findViewById(R.id.titleImage);
+        TextView slideHeading = (TextView) view.findViewById(R.id.texttitle);
+        TextView slideDesciption = (TextView) view.findViewById(R.id.textdeccription);
+
+        slidetitleimage.setImageResource(images[position]);
+        slideHeading.setText(headings[position]);
+        slideDesciption.setText(description[position]);
+
+        container.addView(view);
+
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        container.removeView((LinearLayout)object);
+
+    }
+}
